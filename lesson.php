a19abbb9a80162883e597638d5e53df31af94c65
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-06-07 15:09:24 +0300

    Create lesson.php

---
+++ lesson.php
@@ -0,0 +1,31 @@
+        //получить данные с файла, вывести в массив до = ключ, после значение
+        //раcформировать массив в два массива с формулой и массив с переменной 
+            $filename = __DIR__ . '/file.txt';
+            $fh_arr = file($filename);//читаем содержимое и выводим в массив
+            $actions = [ '*' => 1, '/' => 1, '+' => 1, '-' => 1 ];
+            $arr_value = [];
+        
+            $arr_fn = [];//переменная содержащая функции
+            $arr_variable = [];//переменная содержащая переменные
+
+            foreach($fh_arr as $line) {
+                $line_arr = explode("=", $line); //преобразуем в массив , разделив по знаку = 
+                if (2 != count($line_arr)) continue; 
+               
+                if(fnGetLeftAction($line_arr[1])) {
+                    $arr_fn[$line_arr[0]] = $line_arr[1];
+                } else {
+                    $arr_variable[$line_arr[0]] = $line_arr[1];
+                }
+            }
+
+            //Находим самый левый ключ операции или нуль
+            function fnGetLeftAction(string $variable_or_function_data) {
+                $arr = str_split($variable_or_function_data);
+                global $actions;
+                foreach($arr as $value) {
+                    if (array_key_exists($value, $actions)) {
+                        return $value;
+                    } 
+                }
+            }
